9831b4af734e31b0b335b5580b0fb4cf928da8ea
Ali-Alajaj <alialajaj24@gmail.com>
2026-03-08 23:47:19 +0300

    Enhance server.js with static file serving
    Added static file serving for documentation.

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ import cors from 'cors';
 import clickmart from './routes/clickmart.js';
 import bookly from './routes/bookly.js';
 import errorHandler from './middleware/error.js';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT || 5000
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'documentations')));
 
 server.use('/clickmart', clickmart);
 server.use('/bookly', bookly);
